8daef9628e93110f3c8e54c27d40e772e93ceb16
aicheye <seanxxyang@gmail.com>
2024-05-30 11:20:37 -0400

    Complete regex in Quoridor.java for both pawns

--- src/Board.java
+++ src/Board.java
@@ -15,7 +15,7 @@ public class Board {
 
     // declare variables
     private int[][] squares = new int[9][9];
-    private ArrayList<Wall> walls = new ArrayList<>();
+    private ArrayList<Wall> walls = new ArrayList<Wall>();
     private Pawn p1;
     private Pawn p2;
     private int p1Walls, p2Walls;
@@ -47,9 +47,14 @@ public class Board {
      * Board constructor for loading a game
      * <p>
      * Constructor for building a new Board from a loaded game
-     *
+     * @param p1Pos The position of player 1
+     * @param p2Pos The position of player 2
+     * @param p2Human True if player 2 is human, false if player 2 is computer
+     * @param p1WallsPlaced The number of walls that player 1 has placed
+     * @param p2WallsPlaced The number of walls that player 2 has placed
+     * @param walls The list of walls on the board
      */
-    public Board(int[] p1Pos, int[] p2Pos, boolean p2Human, int p1Walls, int p2Walls, ArrayList<Wall> walls) {
+    public Board(int[] p1Pos, int[] p2Pos, boolean p2Human, int p1WallsPlaced, int p2WallsPlaced, ArrayList<Wall> walls) {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 squares[i][j] = 0;
@@ -65,9 +70,9 @@ public class Board {
         squares[p1.getX()][p1.getY()] = 1;
         squares[p2.getX()][p2.getY()] = 2;
 
-        this.p1Walls = p1Walls;
-        this.p2Walls = p2Walls;
+        this.p1Walls = 10 - p1WallsPlaced;
+        this.p2Walls = 10 - p2WallsPlaced;
 
-        // this.walls = walls;
+        this.walls = new ArrayList<Wall>(walls);
     }
 }
--- src/Quoridor.java
+++ src/Quoridor.java
@@ -10,13 +10,12 @@ import java.io.*;
  */
 public class Quoridor {
     public static void load(String filename) {
-        // initialize constant for filename
-        final String READ_FILE = filename;
-
         // declare variables
+        String line;
+        char[] chars;
         char p1Symbol, p2Symbol;
         boolean p1Human, p2Human;
-        int[][] p1Pos, p2Pos;
+        int[] p1Pos, p2Pos;
         int walls, p1Walls, p2Walls;
         ArrayList<Wall> wallList = new ArrayList<Wall>();
 
@@ -25,11 +24,43 @@ public class Quoridor {
 
         try {
             // initialize BufferedReader object
-            br = new BufferedReader(new FileReader(READ_FILE));
+            br = new BufferedReader(new FileReader(filename));
+
+            // read the first line
+            line = br.readLine();
+
+            // check if the line matches
+            if (line.matches("^[OX]\\s[a-i][1-9]:\\s((Human)|(Computer))$")) {
+                // split the line into characters
+                chars = line.toCharArray();
+
+                // set the symbols, positions, and human status of the players
+                p1Symbol = chars[0];
+                p1Pos = new int[]{chars[2] - 'a', chars[3] - '1'};
+                p1Human = chars[6] == 'H';
+            }
+
+            // read the second line
+            line = br.readLine();
+
+            // check if the line matches
+            if (line.matches("^[OX]\\s[a-i][1-9]:\\s((Human)|(Computer))$")) {
+                // split the line into characters
+                chars = line.toCharArray();
+
+                // set the symbols, positions, and human status of the players
+                p2Symbol = chars[0];
+                p2Pos = new int[]{chars[2] - 'a', chars[3] - '1'};
+                p2Human = chars[6] == 'H';
+            }
         }
 
         catch (IOException e) {
             System.out.println("Error reading file");
         }
+    }
+
+    public static void main(String[] args) {
+        load("./saves/example.txt");
     }
 }
